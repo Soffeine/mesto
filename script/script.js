9be46ec0974454710625d79fdd74fb5602373b5d
Soffeine <Soffein@yandex.ru>
2021-02-27 13:41:28 +0300

    исправить недочеты верстки

--- script/script.js
+++ script/script.js
@@ -1,35 +1,30 @@
 let page = document.querySelector('.page');
-let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.close-button');
-let submitButton = document.querySelector('.edit-form__save-button')
+let editButton = document.querySelector('.profile__edit-button');
+let name = document.querySelector('#name');
+let inputName = document.querySelector('#input-name');
+let description = document.querySelector('#description');
+let inputDescription = document.querySelector('#input-description');
 
 function showEditForm() {
-  popup.classList.add('popup__opened');
-  let name = document.querySelector('#name');
-  let inputName = document.querySelector('#input-name');
+  popup.classList.add('popup_opened');
   inputName.value = name.textContent;
-  let description = document.querySelector('#description');
-  let inputDescription = document.querySelector('#input-description');
   inputDescription.value = description.textContent;
 }
 editButton.addEventListener('click', showEditForm);
 
+let closeButton = document.querySelector('.popup__close-button');
 function closeEditForm() {
-   popup.classList.remove('popup__opened');
+  popup.classList.remove('popup_opened');
 }
 closeButton.addEventListener('click', closeEditForm);
 
+let submitButton = document.querySelector('.edit-form__save-button');
 let editForm = document.querySelector('.edit-form');
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    let name = document.querySelector('#name');
-    let inputName = document.querySelector('#input-name');
-    name.textContent = inputName.value;
-    let description = document.querySelector('#description');
-    let inputDescription = document.querySelector('#input-description');
-    description.textContent = inputDescription.value;
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  name.textContent = inputName.value;
+  description.textContent = inputDescription.value;
   closeEditForm();
 }
 editForm.addEventListener('submit', formSubmitHandler);
-editForm.addEventListener('submit', closeEditForm);
