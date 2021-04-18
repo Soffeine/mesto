732984021d62813167edc3a54cf5d0ad3249487c
Soffeine <Soffein@yandex.ru>
2021-04-19 01:26:37 +0300

    finally works

--- script/validation.js
+++ script/validation.js
@@ -13,29 +13,29 @@ function highlightFieldError(field) {
 }
 
 // микро-функция показа ошибки
-function showError(input) {
-  input.classList.add(validationConfig.popupInputError);
+function showError(input, config) {
+  input.classList.add(config.popupInputError);
 }
 
 //микро-функция скрытия ошибки
-function hideError(input) {
-  input.classList.remove(validationConfig.popupInputError);
+function hideError(input, config) {
+  input.classList.remove(config.popupInputError);
 }
 
-function validateInput (input) {
+function validateInput (input, config) {
     const validity = input.validity;
     const isValid = input.checkValidity();
     if (validity.tooShort || validity.tooLong) { 
-      showError(input, validationConfig.popupInput);
+      showError(input, config);
   } else if (validity.typeMismatch && input.type === 'url') {
-      showError(input, validationConfig.popupInput);
+      showError(input, config);
   } else if (isValid) {
-      hideError(input, validationConfig.popupInput); 
+      hideError(input, config); 
   }
 }
 
 //функция изменения кнопки
-function toggleButtonState(form) {
+function toggleButtonState(form, config) {
     const button = form.querySelector(validationConfig.submitButton);
     const isValid = form.checkValidity();
     if (isValid) {
@@ -49,10 +49,13 @@ function toggleButtonState(form) {
 //функция валидации формы
 function enableValidation(config) {
     const forms = Array.from(document.querySelectorAll(config.popupForm));
+    const formElements = Array.from(document.querySelectorAll(config.popupInput));
     forms.forEach((form) => {
         form.addEventListener('submit', (evt) => {
           evt.preventDefault();
         });
+
+
       setEventListeners(form, config);
   });
 }
@@ -60,19 +63,22 @@ function enableValidation(config) {
 // Обрати внимание - сколько аргументов передали в функцию 
 //(парой строк выше), **ровно столько же** должно быть объявлено при её инициализации.
 function setEventListeners(form, config) {
-    form.addEventListener('input', () => {
-    validateInput(form, config.popupForm);
-    highlightFieldError(form, config.popupInput);
-    toggleButtonState(form, config.popupInput);
+    form.addEventListener('input', (evt) => {
+    const input = evt.target;
+    validateInput(input, config);
+    highlightFieldError(input, config);
+    toggleButtonState(form, config);
   });
  }
 
- enableValidation(validationConfig);
 
-
-
-
-
+ enableValidation({
+    popupForm: '.popup-form',
+    popupInput: '.popup-form__information',
+    popupInputError: 'popup-form__information_error',
+    submitButton: '.popup__button',
+    submitButtonValid: 'popup__button_valid'
+});
 
 
 
